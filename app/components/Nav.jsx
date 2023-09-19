d6e40d879371bc9c69ed762c2399c66f55c8a501
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-09-19 12:29:37 +0200

    Animated Nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import path from "path";
+import { motion as m } from "framer-motion";
 
 const handler = async (req, res) => {
 	const file = path.resolve("/files/BrianCordisco-CV-Web-Developer.pdf");
@@ -11,30 +12,37 @@ const Nav = async () => {
 	const data = await handler();
 
 	return (
-		<div>
-			<div className="nav_background"></div>
-			<nav className="wide_nav_container">
-				<div className="nav_left">
-					<a
-						href="/"
-						className="name">
-						Brian Cordisco
-					</a>
-				</div>
-				<div className="nav_right">
-					<div className="nav_buttons">
-						<Link
-							href={data}
-							target="_blank"
-							rel="noopener noreferrer"
-							locale="false"
-							className="about_button"
-							download>
-							Resume
-						</Link>
+		<div className="nav_bg">
+			<m.div
+				initial={{ y: -65 }}
+				animate={{ y: 0 }}
+				transition={{ duration: 3, type: "spring" }}
+			    		
+			>
+				<div className="nav_background"></div>
+				<nav className="wide_nav_container">
+					<div className="nav_left">
+						<a
+							href="/"
+							className="name">
+							Brian Cordisco
+						</a>
 					</div>
-				</div>
-			</nav>
+					<div className="nav_right">
+						<div className="nav_buttons">
+							<Link
+								href={data}
+								target="_blank"
+								rel="noopener noreferrer"
+								locale="false"
+								className="about_button"
+								download>
+								Resume
+							</Link>
+						</div>
+					</div>
+				</nav>
+			</m.div>
 		</div>
 	);
 };
